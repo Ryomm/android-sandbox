7620ced2f1b8f440980a17c6fc555b17c2eb6d6a
Ryomm <matsusaka.ryoko@nifty.co.jp>
2023-06-26 17:15:58 +0900

    add view

--- hilt/app/src/main/java/com/example/hiltandmultimodule/MainActivity.kt
+++ hilt/app/src/main/java/com/example/hiltandmultimodule/MainActivity.kt
@@ -5,7 +5,15 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,8 +22,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.hiltandmultimodule.ui.theme.HiltAndMultimoduleTheme
@@ -179,7 +190,35 @@ fun ArticlesScreen(
 fun Article(
     article: ArticleLocalModel
 ) {
-    Text(text = article.title, style = MaterialTheme.typography.titleMedium)
+    Card {
+        Text(
+            text = article.title,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(8.dp)
+        )
+        Text(
+            text = article.url,
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(8.dp)
+        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Icon(
+                painter = rememberVectorPainter(image = Icons.Filled.Favorite),
+                contentDescription = null,
+                modifier = Modifier.padding(2.dp)
+            )
+            Text(text = article.likesCount.toString(), style = MaterialTheme.typography.labelMedium)
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(text = article.user.name, style = MaterialTheme.typography.labelMedium, modifier = Modifier.padding(end = 0.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
